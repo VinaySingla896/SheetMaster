b81e68d7294b94dfc8459de8483f10ea0fdf9182
VinaySingla896 <vinay.s@wizcommmerce.com>
2025-03-02 17:12:05 +0530

    Made some improvements

--- client/src/components/spreadsheet/FormulaBar.tsx
+++ client/src/components/spreadsheet/FormulaBar.tsx
@@ -9,12 +9,12 @@ interface FormulaBarProps {
 export function FormulaBar({ value, onChange }: FormulaBarProps) {
   return (
     <div className="border-b border-gray-300 p-2 flex items-center gap-2 bg-white">
-      <div className="text-gray-500">fx</div>
+      <div className="text-gray-500">text</div>
       <Input
         value={value}
         onChange={(e) => onChange(e.target.value)}
         className="flex-grow"
-        placeholder="Enter a value or start with = for formulas"
+        placeholder=""
       />
       <FormulaHelp />
     </div>
